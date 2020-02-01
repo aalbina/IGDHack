11d6b96f9a14725ab1f5c8c5adbda45a7487455e
YurBurGer <yurger2009@gmail.com>
2020-02-02 00:05:41 +0300

    added segmented display for gas

--- InnoHackContoller/InnoHackContoller.ino
+++ InnoHackContoller/InnoHackContoller.ino
@@ -2,6 +2,9 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "math.h"
 #include "rgb_lcd.h"
+#include <Arduino.h>
+#include <TM1637Display.h>
+
 
 MPU6050 mpu;
 #define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
@@ -24,6 +27,18 @@ void dmpDataReady() {
     mpuInterrupt = true;
 }
 
+// Module connection pins (Digital Pins)
+#define CLK 4
+#define DIO 3
+TM1637Display display(CLK, DIO);
+const byte minus_seg[1]={SEG_G};
+void drawGas(byte gas,bool reverse){
+  display.clear();
+  display.showNumberDec(gas, true,3,1);
+  if(reverse){
+    display.setSegments(minus_seg,1,0);
+  }
+}
 rgb_lcd lcd;
 byte black_square[8] = {
     0b11111,
@@ -57,12 +72,19 @@ byte fuelLevel=0;
 
 void setup() {
   Serial.begin(115200);
+
   lcd.begin(16, 2);
   lcd.createChar(0, black_square);
-
   setlcdlevel(6);
   lcd.setRGB(40, 120, 40);
+
+  
+  display.setBrightness(0x0f);
+  //add reverse read before;
+  drawGas(100,false);
+  
   pinMode(LED_PIN, OUTPUT); 
+  
   Wire.begin();
   Wire.setClock(400000);
   Serial.println(F("Initializing I2C devices..."));
@@ -73,49 +95,48 @@ void setup() {
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
 
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
 
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // Calibration Time: generate offsets and calibrate our MPU6050
-        mpu.CalibrateAccel(6);
-        mpu.CalibrateGyro(6);
-        mpu.PrintActiveOffsets();
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+      // Calibration Time: generate offsets and calibrate our MPU6050
+      mpu.CalibrateAccel(6);
+      mpu.CalibrateGyro(6);
+      mpu.PrintActiveOffsets();
+      // turn on the DMP, now that it's ready
+      Serial.println(F("Enabling DMP..."));
+      mpu.setDMPEnabled(true);
 
-        // enable Arduino interrupt detection
-        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-        Serial.println(F(")..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
+      // enable Arduino interrupt detection
+      Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+      Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+      Serial.println(F(")..."));
+      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
 
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      dmpReady = true;
 
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-    //del=millis();
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+  }
 }
 
 void loop() {
@@ -130,16 +151,17 @@ void loop() {
         //other program here
         //Serial.println();
         
+        //add reverse read
+        
+        //add enable read
+        
         //gas angle management
         angle=int(round(ypr[0] * 180/M_PI));
         angle=constrain(angle,-65,-5);
         angle=map(angle,-65,-5,0,100);
+        drawGas(angle,false);
         Serial.print("g:");
         Serial.println(angle);
-        
-        //add reverse read
-        
-        //add enable read
 
         //reading management
         if(Serial.available()>0){
